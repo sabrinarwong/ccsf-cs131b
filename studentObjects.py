46f02c90d9c4115c2676f330fed03f966b8608b9
Sabrina Wong <sabrina.r.wong@gmail.com>
2019-10-28 23:09:43 -0700

    lab8

--- studentObjects.py
+++ studentObjects.py
@@ -2,10 +2,11 @@
 # @Author: sabrina
 # @Date:   2019-09-11 19:36:07
 # @Last Modified by:   Sabrina Wong
-# @Last Modified time: 2019-10-28 22:20:23
+# @Last Modified time: 2019-10-28 23:09:11
+
 
 # 1. Create an object specification with these seven data fields: name, address, city, state, zip, student id, and gpa. 
-class studentObject:
+class Student:
 	name = None
 	address = None
 	city = None
@@ -14,36 +15,36 @@ class studentObject:
 	student_id = None
 	gpa = None
 
-	# 3. Write a value-returning function to create and return a single student object. Design console prompts and input statements in the function for the eight data fields. Call the function three times – once to initialize each of main’s student objects. 
-	def __init__(self, name, address, city, state, zipcode, sid, gpa):
-		self.name = name
-		self.address = address
-		self.city = city
-		self.state = state
-		self.zip_code = zipcode
-		self.student_id = sid
-		self.gpa = gpa
+	# 3a. Write a value-returning function to create and return a single student object. Design console prompts and input statements in the function for the eight data fields.
+	def __init__(self, name=None, address=None, city=None, state=None, zipcode=None, sid=None, gpa=None):
+		self.name = (name or input("Enter your name: "))
+		self.address = (address or input("Enter your address: "))
+		self.city = (city or input("Enter your city: "))
+		self.state = (state or input("Enter your state: "))
+		self.zip_code = (zipcode or input("Enter your zipcode: "))
+		self.student_id = (sid or input("Enter your sid: "))
+		self.gpa = (gpa or input("Enter your gpa: "))
 
-	# 4. Write a void function to output nicely formatted labels and values for a single student object’s data fields. Call the function three times – once for each student object.
-	def to_String(self):
-		print("name:", self.name, '\n')
-		print("address:", self.address, '\n')
-		print("city:", self.city, '\n')
+		print("\n")
 
+	# 4a. Write a void function to output nicely formatted labels and values for a single student object’s data fields.
+	def print(self):
+		print("name: ${self.name}", 
+			  "address: ${self.address}", 
+			  "city: ${self.city}", 
+			  "state: ${self.state}", 
+			  "zip code: ${self.zip_code}", 
+			  "student id: ${self.student_id}", 
+			  "gpa: ${self.gpa}", sep="\n", end="\n")
 
-
-	# 2. In main, declare 3 uninitialized student objects, using either by making a list of 3 objects or 3 individual objects -- your choice. 
-	def __main__(* args):
-		students_list = [studentObject(), studentObject(), studentObject()]
-		student1 = studentObject("Joe Student", "123 Long Road", "Happytown", "CA", 98765, 2.64)
-
+# 2. In main, declare 3 uninitialized student objects, using either by making a list of 3 objects or 3 individual objects -- your choice. 
 if __name__ == '__main__':
-	main(self)
+	# 3b. Call the function three times – once to initialize each of main’s student objects. 
+	students_list = [Student(), Student(), Student()]
 
-
-# 3. Write a value-returning function to create and return a single student object. Design console prompts and input statements in the function for the eight data fields. Call the function three times – once to initialize each of main’s student objects. 
-
-
+	# 4b. Call the function three times – once for each student object.
+	for i in students_list:
+		i.print()
 
 
 # END OF PROGRAM
